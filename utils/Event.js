5897bebb9048fcf4586550d1ea00c95d834ec7ec
João Maia <jpmmaia@gmail.com>
2015-12-25 13:03:19 +0000

    Refactoring on Utils/Event.js

--- utils/Event.js
+++ utils/Event.js
@@ -2,11 +2,6 @@ Utils.Event = function()
 {
     this.initialize();
 };
-Utils.Event.EventPair = function(subscriber, handler)
-{
-    this.subscriber = subscriber;
-    this.handler = handler;
-};
 
 Utils.Event.prototype.initialize = function()
 {
@@ -15,19 +10,15 @@ Utils.Event.prototype.initialize = function()
 
 Utils.Event.prototype.subscribe = function(subscriber, handler)
 {
-    this.subscribers.add(new Utils.Event.EventPair(subscriber, handler));
+    this.subscribers.add(Utils.createDelegate(subscriber, handler));
 };
 Utils.Event.prototype.unsubscribe = function(subscriber, handler)
 {
-    this.subscribers.remove(new Utils.Event.EventPair(subscriber, handler));
+    this.subscribers.remove(Utils.createDelegate(subscriber, handler));
 };
 
 Utils.Event.prototype.raise = function(sender, eventArguments)
 {
     for(var i = 0; i < this.subscribers.size(); i++)
-    {
-        var pair = this.subscribers.get(i);
-        var delegate = Utils.createDelegate(pair.subscriber, pair.handler);
-        delegate(sender, eventArguments);
-    }
+        this.subscribers.get(i)(sender, eventArguments);
 };
